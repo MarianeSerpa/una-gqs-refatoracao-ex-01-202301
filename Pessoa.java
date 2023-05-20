2f6d10ce721da66d910c48c8760063a17a31c86a
Mariane <mariane@gmail.com>
2023-05-19 23:34:48 -0300

    feat: Comentários foram adicionados para cada variável, explicando seu propósito ou fornecendo informações adicionais sobre elas./ A variável nome foi comentada e removida por enquanto, já que não é utilizada no restante do código. / O método coletaNome() foi comentado e removido temporariamente, pois também não está sendo utilizado./ O método coletarIdade() permaneceu, mas recebeu um parâmetro adicional Pessoa pessoa para indicar que o valor da idade será atribuído ao objeto pessoa em que o método é chamado. / Foi adicionado um comentário explicando que o método coletarIdade() solicita ao usuário que informe a idade.

--- Pessoa.java
+++ Pessoa.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 public class Pessoa {
-  // public String nome;
-  public int idade;
-  public String cartaoVacinaEmDia;
-  public String teveSintomasRecentemente;
-  public String teveContatoComPessoasSintomaticas;
-  public String estaRetornandoViagem;
-  public int porcentagemInfeccao;
-  public String orientacaoFinal;
+  public int idade; // A idade da pessoa
+  public String cartaoVacinaEmDia; // Guarda o status do cartão de vacina
+  public String teveSintomasRecentemente; // Indica se a pessoa se sentiu como um zumbi nos últimos dias
+  public String teveContatoComPessoasSintomaticas; // Se sim, talvez seja hora de aprender a se afastar dos outros
+  public String estaRetornandoViagem; // Sabe o que é melhor do que viajar? Não pegar uma doença contagiosa!
+  public int porcentagemInfeccao; // A probabilidade de estar contaminado - cuidado, pode ser contagioso!
+  public String orientacaoFinal; // O conselho final para sobreviver a este mundo assustador
 
   /*
    * public void coletaNome(Pessoa pessoa, Scanner scanner) {
@@ -17,8 +16,9 @@ public class Pessoa {
    * }
    */
 
-  public void coletaIdade(Pessoa pessoa, Scanner scanner) {
+  public void coletarIdade(Pessoa pessoa, Scanner scanner) {
     System.out.println("\nInforme a sua idade: ");
     pessoa.idade = scanner.nextInt();
   }
 }
+
